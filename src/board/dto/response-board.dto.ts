7b7e0beec4b0a3c968bb98107a8072480efe24c9
rudwk <tlsruwk12345@gmail.com>
2026-02-06 16:31:15 +0900

    feature: dto 생성

---
+++ src/board/dto/response-board.dto.ts
@@ -0,0 +1,14 @@
+export namespace BoardResponseDto{
+  export class list {
+    title: string;
+    boardColor: string;
+    isMaster: boolean;
+    authorName: string;
+    tripDate: Date;
+    authorProfileImage: string;
+  }
+
+  export class detail {
+    
+  }
+}
