9041f867a8c2a96eed14c1a249b02485903554c8
rudwk <tlsruwk12345@gmail.com>
2026-02-09 12:05:50 +0900

    refactor: jwt요소 변경
    userId -> id

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   async validate(payload: any) {
     return {
-      userId: payload.sub,
+      id: payload.sub,
       role: payload.role,
     };
   }
